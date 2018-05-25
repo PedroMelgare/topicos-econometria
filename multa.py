b4d4bc63a9a482543cbee7a35334d00a33485914
PedroMelgare <33072878+PedroMelgare@users.noreply.github.com>
2018-05-25 20:19:22 -0300

    Rename multa.txt to multa.py

--- multa.py
+++ multa.py
@@ -4,6 +4,6 @@ v = float(input("Insira a velocidade, em km/h:"))
 #Processamento
 if v > 80:
 	multa = 5*(v-80)
-	print("Você foi multado em R$", multa)
+	print("VocÃª foi multado em R$", multa)
 
 
