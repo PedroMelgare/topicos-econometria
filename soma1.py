5bc4e89ce98980aa04ba7d75f6e8afa9d6758a57
PedroMelgare <33072878+PedroMelgare@users.noreply.github.com>
2018-05-25 20:22:56 -0300

    Update soma1.py

--- soma1.py
+++ soma1.py
@@ -1,3 +1,10 @@
+"""
+Programa soma1.py
+Descrição: Entrada, processamento e saída de dados em Python
+Autor: Pedro de Bitencourt Melgare
+Data: 18/05/2018
+Versão: 0.0.1
+"""
 #Entrada de dados
 a = 1
 
