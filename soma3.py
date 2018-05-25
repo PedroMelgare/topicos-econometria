9b3134c5e6e33c3e446fb7dfe9e1f98f0e36d526
PedroMelgare <33072878+PedroMelgare@users.noreply.github.com>
2018-05-25 20:23:39 -0300

    Update soma3.py

--- soma3.py
+++ soma3.py
@@ -1,3 +1,10 @@
+"""
+Programa soma3.py
+Descrição: Entrada, processamento e saída de dados em Python
+Autor: Pedro de Bitencourt Melgare
+Data: 18/05/2018
+Versão: 0.0.1
+"""
 #Definição de funções
 def soma(x,y):
 	soma=x+y
