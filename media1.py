4cff6dd0f792461b713bcd2321f381de6b307d7e
PedroMelgare <33072878+PedroMelgare@users.noreply.github.com>
2018-05-25 20:21:46 -0300

    Update media1.py

--- media1.py
+++ media1.py
@@ -1,3 +1,11 @@
+"""
+Programa media1.py
+Descrição: Entrada, processamento e saída de dados em Python
+Autor: Pedro de Bitencourt Melgare
+Data: 18/05/2018
+Versão: 0.0.1
+"""
+
 #Entrada de dados
 a = 2
 
